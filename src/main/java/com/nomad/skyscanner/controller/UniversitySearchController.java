4e18f38f85986292ed00419fcf486b9fd0c203ce
Yiping Deng <yiping.deng.98@gmail.com>
2018-11-04 09:48:53 +0100

    minor changes

--- src/main/java/com/nomad/skyscanner/controller/UniversitySearchController.java
+++ src/main/java/com/nomad/skyscanner/controller/UniversitySearchController.java
@@ -55,15 +55,80 @@ public class UniversitySearchController {
 
     @GetMapping("/add_test_data")
     public University addData(){
+        // ---- test uni 1
         University university = new University();
-        university.setName("Uni Bremen");
+        university.setName("Jacobs University");
         university.setLocation_code("bre");
+
         Vacation vacation = new Vacation();
-        vacation.setStart_date(LocalDate.now().plusDays(3));
-        vacation.setEnd_date(LocalDate.now().plusDays(6));
-        vacation.setDay_of_vacation(3);
+        vacation.setStart_date(LocalDate.parse("2018-12-22"));
+        vacation.setEnd_date(LocalDate.parse("2018-12-30"));
+        vacation.setDay_of_vacation(8);
+
+        Vacation vacation1 = new Vacation();
+        vacation1.setStart_date(LocalDate.parse("2018-11-20"));
+        vacation1.setEnd_date(LocalDate.parse("2018-11-25"));
+        vacation1.setDay_of_vacation(5);
+
+        Vacation vacation2 = new Vacation();
+        vacation2.setStart_date(LocalDate.parse("2019-01-01"));
+        vacation2.setEnd_date(LocalDate.parse("2019-01-30"));
+
         List<Vacation> vList = new ArrayList<>();
         vList.add(vacation);
+        vList.add(vacation1);
+        vList.add(vacation2);
+        university.setAvailable_vacations(vList);
+        universityRepository.save(university);
+
+        university = new University();
+        university.setName("Uni Bremen");
+        university.setLocation_code("bre");
+
+        vacation = new Vacation();
+        vacation.setStart_date(LocalDate.parse("2018-12-20"));
+        vacation.setEnd_date(LocalDate.parse("2018-12-26"));
+        vacation.setDay_of_vacation(8);
+
+        vacation1 = new Vacation();
+        vacation1.setStart_date(LocalDate.parse("2018-11-10"));
+        vacation1.setEnd_date(LocalDate.parse("2018-11-15"));
+        vacation1.setDay_of_vacation(5);
+
+        vacation2 = new Vacation();
+        vacation2.setStart_date(LocalDate.parse("2019-01-04"));
+        vacation2.setEnd_date(LocalDate.parse("2019-01-20"));
+
+        vList = new ArrayList<>();
+        vList.add(vacation);
+        vList.add(vacation1);
+        vList.add(vacation2);
+        university.setAvailable_vacations(vList);
+        universityRepository.save(university);
+
+
+        university = new University();
+        university.setName("Hamburg University");
+        university.setLocation_code("HAMB");
+
+        vacation = new Vacation();
+        vacation.setStart_date(LocalDate.parse("2018-12-09"));
+        vacation.setEnd_date(LocalDate.parse("2018-12-28"));
+        vacation.setDay_of_vacation(8);
+
+        vacation1 = new Vacation();
+        vacation1.setStart_date(LocalDate.parse("2018-11-26"));
+        vacation1.setEnd_date(LocalDate.parse("2018-11-30"));
+        vacation1.setDay_of_vacation(5);
+
+        vacation2 = new Vacation();
+        vacation2.setStart_date(LocalDate.parse("2019-01-15"));
+        vacation2.setEnd_date(LocalDate.parse("2019-01-20"));
+
+        vList = new ArrayList<>();
+        vList.add(vacation);
+        vList.add(vacation1);
+        vList.add(vacation2);
         university.setAvailable_vacations(vList);
         return universityRepository.save(university);
     }
